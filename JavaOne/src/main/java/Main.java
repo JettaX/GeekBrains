6d677ca10ebc80d61aeffb6b7fb7f83def4e4386
Vadim <vadim91111@gmail.com>
2022-01-11 12:52:53 +0300

    JavaOne HW2 done

---
+++ JavaOne/src/main/java/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isAmountWithin(5, 10));
+        printPositiveOrNegative(-1);
+        System.out.println(isPositive(-1));
+        printStringNTimes("GeekBrains", 3);
+        System.out.println(isLeapYear(2060));
+    }
+
+    public static boolean isAmountWithin(int numberOne, int numberTwo) {
+        int sum = numberOne + numberTwo;
+        return sum >= 10 && sum <= 20;
+    }
+
+    public static void printPositiveOrNegative(int number) {
+        System.out.println(number >= 0 ? "положительное" : "отрицательное");
+    }
+
+    public static boolean isPositive(int number) {
+        return number >= 0;
+    }
+
+    public static void printStringNTimes(String string, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(string);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else return year % 4 == 0;
+    }
+}
